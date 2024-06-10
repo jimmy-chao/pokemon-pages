7d12996b484b42b57278b8e93d188f6a90e475ed
Jimmy <jimmychao.1@gmail.com>
2024-06-10 12:14:52 -0700

    first commit

---
+++ src/Pagination.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Pagination({ gotoPreviousPage, gotoNextPage }) {
+  return (
+    <div>
+      {gotoPreviousPage && <button onClick={gotoPreviousPage}>Previous</button>}
+      {gotoNextPage && <button onClick={gotoNextPage}>Next</button>}
+    </div>
+  );
+}
